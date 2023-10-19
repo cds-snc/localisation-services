d1f54bb10cfbc3d604484dc07a03ef2a47ed641b
Pat Heard <patrick.heard@cds-snc.ca>
2023-10-19 15:32:57 +0000

    feat: add load balancer Terraform module
    Add the Terraform module for the `load_balancer`.

---
+++ terraform/aws/load_balancer/kinesis.tf
@@ -0,0 +1,82 @@
+#
+# Kinesis Firehose
+#
+locals {
+  cbs_satellite_bucket_arn    = "arn:aws:s3:::${var.cbs_satellite_bucket_name}"
+  cbs_satellite_bucket_prefix = "waf_acl_logs/AWSLogs/${var.account_id}/"
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "firehose_waf_logs" {
+  name        = "aws-waf-logs-localisation-${var.region}"
+  destination = "extended_s3"
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  extended_s3_configuration {
+    role_arn           = aws_iam_role.firehose_waf_logs.arn
+    prefix             = local.cbs_satellite_bucket_prefix
+    bucket_arn         = local.cbs_satellite_bucket_arn
+    compression_format = "GZIP"
+  }
+
+  tags = var.common_tags
+}
+
+#
+# IAM
+#
+resource "aws_iam_role" "firehose_waf_logs" {
+  name               = "FirehoseWafLogs"
+  assume_role_policy = data.aws_iam_policy_document.firehose_assume.json
+}
+
+resource "aws_iam_policy" "firehose_waf_logs" {
+  name   = "FirehoseWafLogsPolicy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.firehose_waf_logs.json
+}
+
+resource "aws_iam_role_policy_attachment" "firehose_waf_logs" {
+  role       = aws_iam_role.firehose_waf_logs.name
+  policy_arn = aws_iam_policy.firehose_waf_logs.arn
+}
+
+data "aws_iam_policy_document" "firehose_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "firehose_waf_logs" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject"
+    ]
+    resources = [
+      local.cbs_satellite_bucket_arn,
+      "${local.cbs_satellite_bucket_arn}/*"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:CreateServiceLinkedRole"
+    ]
+    resources = [
+      "arn:aws:iam::*:role/aws-service-role/wafv2.amazonaws.com/AWSServiceRoleForWAFV2Logging"
+    ]
+  }
+}
---
+++ terraform/aws/load_balancer/load_balancer.tf
@@ -0,0 +1,110 @@
+resource "aws_lb" "localisation" {
+  name               = "localisation-${var.env}"
+  internal           = false
+  load_balancer_type = "application"
+
+  drop_invalid_header_fields = true
+  enable_deletion_protection = true
+
+  security_groups = [
+    var.security_group_lb_id
+  ]
+  subnets = var.subnet_public_ids
+
+  access_logs {
+    bucket  = var.cbs_satellite_bucket_name
+    prefix  = "lb_logs"
+    enabled = true
+  }
+
+  tags = var.common_tags
+}
+
+resource "random_string" "alb_tg_suffix" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "aws_lb_target_group" "localisation" {
+  name                 = "localisation-tg-${random_string.alb_tg_suffix.result}"
+  port                 = 4443
+  protocol             = "HTTPS"
+  target_type          = "ip"
+  deregistration_delay = 30
+  vpc_id               = var.vpc_id
+
+  health_check {
+    enabled             = true
+    interval            = 10
+    protocol            = "HTTPS"
+    path                = "/"
+    matcher             = "200-399"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  stickiness {
+    type = "lb_cookie"
+  }
+
+  tags = var.common_tags
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes = [
+      stickiness[0].cookie_name
+    ]
+  }
+}
+
+resource "aws_lb_listener" "localisation" {
+  load_balancer_arn = aws_lb.localisation.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.localisation.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.localisation.arn
+  }
+
+  depends_on = [
+    aws_acm_certificate_validation.localisation,
+    aws_route53_record.localisation_validation,
+  ]
+}
+
+resource "aws_alb_listener_rule" "localisation" {
+  listener_arn = aws_lb_listener.localisation.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.localisation.arn
+  }
+
+  condition {
+    host_header {
+      values = [var.domain]
+    }
+  }
+}
+
+resource "aws_lb_listener" "localisation_http_redirect" {
+  load_balancer_arn = aws_lb.localisation.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
